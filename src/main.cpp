5114fa9ae41fc63805721fcd66f0421c8e40c290
Yuki Koyama <yuki@koyama.xyz>
2022-05-03 17:46:56 +0900

    Define particles

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,25 @@
+#include <Eigen/Core>
+#include <iostream>
+#include <vector>
+
+using Vec3 = Eigen::Vector3d;
+
+struct Particle
 {
-int main() { return 0; }
+    double m;
+    Vec3   x;
+    Vec3   p;
+    Vec3   v;
+};
+
+int main()
+{
+    std::vector<Particle> particles{20};
+
+    for (auto& particle : particles)
+    {
+        std::cout << particle.x.transpose() << std::endl;
+    }
+
+    return 0;
+}
