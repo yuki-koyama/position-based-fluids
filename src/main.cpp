d9883603237f087ebdd498b8feb44656f4d3c669
Yuki Koyama <yuki@koyama.xyz>
2022-05-04 00:30:29 +0900

    Add a tentative initialization code

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,24 @@ struct Particle
 
 int main()
 {
-    std::vector<Particle> particles{20};
+    std::vector<Particle> particles;
 
+    constexpr int x_size = 6;
+    constexpr int y_size = 6;
+    constexpr int z_size = 6;
+
+    // Generate and initialize particles
+    constexpr int num_particles = x_size * y_size * z_size;
+    particles.resize(num_particles);
+    for (int i = 0; i < num_particles; ++i)
+    {
+        particles[i].i = i;
+        particles[i].m = 1.0 / static_cast<Scalar>(num_particles);
+        particles[i].x = Vec3::Random();
+        particles[i].v = Vec3::Zero();
+    }
+
+    // Print particle status
     for (auto& particle : particles)
     {
         std::cout << particle.x.transpose() << std::endl;
