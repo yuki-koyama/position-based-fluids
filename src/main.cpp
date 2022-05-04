d1c6cfe827c1c05e492fde81175b687f4df2e4c0
Yuki Koyama <yuki@koyama.xyz>
2022-05-04 10:29:06 +0900

    Add alembic manager for exporting particle positions

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <Alembic/AbcCoreOgawa/All.h>
+#include <Alembic/AbcGeom/All.h>
 #include <Eigen/Core>
 #include <cstdint>
 #include <iostream>
@@ -17,9 +19,106 @@ struct Particle
     std::vector<int> neighbors;
 };
 
+class AlembicManagerBase
+{
+public:
+    AlembicManagerBase(const std::string& output_file_path, const double delta_time, const std::string& object_name)
+        : m_archive(Alembic::AbcCoreOgawa::WriteArchive(), output_file_path.c_str())
+    {
+        using namespace Alembic::Abc;
+        using namespace Alembic::AbcGeom;
+
+        const TimeSampling  time_sampling(delta_time, 0);
+        const std::uint32_t time_sampling_index = m_archive.addTimeSampling(time_sampling);
+
+        m_points = OPoints(OObject(m_archive, kTop), object_name.c_str());
+        m_points.getSchema().setTimeSampling(time_sampling_index);
+    }
+
+    void submitCurrentStatus()
+    {
+        using namespace Alembic::Abc;
+        using namespace Alembic::AbcGeom;
+
+        if (m_is_first)
+        {
+            submitCurrentStatusFirstTime();
+
+            m_is_first = false;
+
+            return;
+        }
+
+        const OPointsSchema::Sample sample(V3fArraySample((const V3f*) getAlembicPositionArray(), getNumVerts()));
+
+        m_points.getSchema().set(sample);
+    }
+
+protected:
+    virtual const float* getAlembicPositionArray()      = 0;
+    virtual std::int32_t getNumVerts() const            = 0;
+    virtual void         submitCurrentStatusFirstTime() = 0;
+
+    bool m_is_first = true;
+
+    Alembic::Abc::OArchive    m_archive;
+    Alembic::AbcGeom::OPoints m_points;
+};
+
+class ParticlesAlembicManager : public AlembicManagerBase
+{
+public:
+    ParticlesAlembicManager(const std::string&           output_file_path,
+                            const double                 delta_time,
+                            const std::string&           object_name,
+                            const std::vector<Particle>* particles_ptr)
+        : AlembicManagerBase(output_file_path, delta_time, object_name), m_particles_ptr(particles_ptr)
+    {
+        m_position_buffer.resize(3, getNumVerts());
+    }
+
+private:
+    const float* getAlembicPositionArray() override
+    {
+        for (std::size_t index = 0; index < getNumVerts(); ++index)
+        {
+            m_position_buffer.col(index) = (*m_particles_ptr)[index].x.cast<float>();
+        }
+        return m_position_buffer.data();
+    }
+
+    std::int32_t getNumVerts() const override { return m_particles_ptr->size(); }
+
+    void submitCurrentStatusFirstTime() override
+    {
+        using namespace Alembic::Abc;
+        using namespace Alembic::AbcGeom;
+
+        const std::size_t               num_counts = getNumVerts();
+        const std::vector<std::int32_t> counts(num_counts, 1);
+
+        std::vector<uint64_t> index_buffer(num_counts);
+        for (std::size_t elem_index = 0; elem_index < num_counts; ++elem_index)
+        {
+            index_buffer[elem_index] = elem_index;
+        }
+
+        const OPointsSchema::Sample sample(V3fArraySample((const V3f*) getAlembicPositionArray(), getNumVerts()),
+                                           UInt64ArraySample(index_buffer.data(), getNumVerts()));
+
+        m_points.getSchema().set(sample);
+    }
+
+    const std::vector<Particle>* m_particles_ptr;
+
+    Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> m_position_buffer;
+};
+
 int main()
 {
     std::vector<Particle> particles;
+
+    constexpr Scalar dt = 1.0 / 60.0;
 
     constexpr int x_size = 6;
     constexpr int y_size = 6;
@@ -35,6 +134,9 @@ int main()
         particles[i].x = Vec3::Random();
         particles[i].v = Vec3::Zero();
     }
+
+    ParticlesAlembicManager alembic_manager("./test.abc", dt, "fluid", &particles);
+    alembic_manager.submitCurrentStatus();
 
     // Print particle status
     for (auto& particle : particles)
